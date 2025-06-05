e90ee437907a09c2ef0ec9c69a7219587fd65cee
adsfasdgfascv <23104452@usc.edu.ph>
2025-06-05 13:56:57 +0800

    Add MyDataApp/app/src/main/java/com/example/mydataapp/ItemAdapter.java 1749103015733

---
+++ MyDataApp/app/src/main/java/com/example/mydataapp/ItemAdapter.java
@@ -0,0 +1,65 @@
+package com.example.mydataapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+
+    private List<Item> itemList;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public ItemAdapter(List<Item> itemList, OnItemClickListener listener) {
+        this.itemList = itemList;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_row, parent, false);
+        return new ItemViewHolder(itemView, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        holder.textViewTitle.setText(item.getTitle());
+        holder.textViewDescription.setText(item.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView textViewTitle, textViewDescription;
+        OnItemClickListener listener;
+
+        public ItemViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+            this.listener = listener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            listener.onItemClick(getAdapterPosition());
+        }
+    }
+}
