26a957d48a6dca0991b93122e8cfb21549f50810
adsfasdgfascv <23104452@usc.edu.ph>
2025-06-05 13:57:07 +0800

    Add MyDataApp/app/src/main/java/com/example/mydataapp/Item.java 1749103025518

---
+++ MyDataApp/app/src/main/java/com/example/mydataapp/Item.java
@@ -0,0 +1,50 @@
+package com.example.mydataapp;
+
+public class Item {
+    private int id;
+    private String title;
+    private String description;
+    private int userId;
+
+    public Item() {
+    }
+
+    public Item(int id, String title, String description, int userId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
